a84b8b172d1123b4569fe6e53cbbc539d53e5bcd
asd15079056809 <693315348@qq.com>
2020-04-11 15:07:56 +0800

    项目初始结构创建

---
+++ gulimall-coupon/src/test/java/com/bdqn/coupon/GulimallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bdqn.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ gulimall-ware/src/test/java/com/bdqn/ware/GulimallWareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bdqn.ware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallWareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
